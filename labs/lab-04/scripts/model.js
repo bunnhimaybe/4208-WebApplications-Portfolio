7113de4cb846ef1a9a0cd414297bf47ebef15ccf
teenhi <lisanapham@gmail.com>
2025-09-29 21:38:03 -0500

    add lab 4 goal 3

--- labs/lab-04/scripts/model.js
+++ labs/lab-04/scripts/model.js
@@ -1,21 +1,42 @@
 /* Game data */
 
 const passcode = Math.floor( Math.random() * 1000);
-let tries = 10; 
 const guess = new Guess();
+let then = Date.now();
+let timeLeft = 30;
+let gameover = false;
+let tries = 10; 
 
 
 
 /* Game logic */ 
 
+function main() {
+    const now = Date.now();
+    if (gameover) {
+        return;
+    } else if (timeLeft <= 0) {
+        printGameOver('LOSE');
+    } else if (now - then > 1000) {
+        timeLeft--;
+        printDigits();
+        printAttemptsRemaining();
+        then = Date.now();
+    }
+    requestAnimationFrame(main);
+}
+
+main();
+
 function guessNumber(guess) {
     tries--;
     if (guess == passcode) {
-        printGameOver('WIN'); 
-    } else if (tries <= 0 ) {
-        printGameOver('LOSE');
+        gameover = true;
+        printGameOver('WIN');         
+    // } else if (tries <= 0) {
+    //     printGameOver('LOSE');
     } else {
-        printAttemptsRemaining(tries);
+        // printAttemptsRemaining(tries);
         giveClue(guess);
     }
 }
--- labs/lab-04/scripts/view.js
+++ labs/lab-04/scripts/view.js
@@ -1,26 +1,36 @@
 /* Game outputs */
 
 function printDigits() {
-    document.getElementById('digit-100').value = guess.hundreds;
-    document.getElementById('digit-10').value = guess.tens;
-    document.getElementById('digit-1').value = guess.ones;
+    document.getElementById('digit-100').src = `assets/${guess.hundreds}.png`;
+    document.getElementById('digit-10').src = `assets/${guess.tens}.png`;
+    document.getElementById('digit-1').src = `assets/${guess.ones}.png`;
 }
 
 function printAttemptsRemaining(tries) {
     const attemptsText = document.getElementById('attempts');
-    attemptsText.innerHTML = `Number of attempts left: ${tries}`;
+    // attemptsText.innerHTML = `Number of attempts left: ${tries}`;
+    attemptsText.innerHTML = `<h2> Time Left: ${timeLeft} </h2>`;
 }
 
 function printClue(status, guess) {
-    const clueText = document.getElementById('clues');
-    const clue = (status === 'HI') ? `<li> ${guess} is too high! </li>` : `<li> ${guess} is too low! </li>`;
-    clueText.innerHTML += clue;
+    // const clueText = document.getElementById('clues');
+    // const clue = (status === 'HI') ? `<li> ${guess} is too high! </li>` : `<li> ${guess} is too low! </li>`;
+    // clueText.innerHTML += clue;
+
+    const [digit100, digit10, digit1] 
+        = status == 'HI' ? ['H', 'I', '-'] 
+        : ['L', 'O', '-'];
+    document.getElementById('digit-100').src = `assets/${digit100}.png`;
+    document.getElementById('digit-10').src = `assets/${digit10}.png`;
+    document.getElementById('digit-1').src = `assets/${digit1}.png`;
+    then = Date.now();
 }
 
 function printGameOver(status) {
     if (status === 'WIN') {
-        var message = `<h1> You Win! </h1>
-            <p> Got it in ${10-tries} tries. </p>`
+        var message = `<h1> You Win! </h1> 
+            <p> Got it in ${30 - timeLeft} seconds. </p>`;
+            // <p> Got it in ${10-tries} tries. </p>`
     } else {
         var message = `<h1> You Lose! </h1>
             <p> The number was ${passcode}</p>`;
