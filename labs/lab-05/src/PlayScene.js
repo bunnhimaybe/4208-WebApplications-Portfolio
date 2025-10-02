0a488d985f6a508c5987c681c5dc45f29afc0783
teenhi <lisanapham@gmail.com>
2025-10-01 23:07:01 -0500

    add lab 05 full build

--- labs/lab-05/src/PlayScene.js
+++ labs/lab-05/src/PlayScene.js
@@ -2,28 +2,43 @@ class PlayScene extends Phaser.Scene {
     // construct new scene
     constructor() {
         super('play');      // set this scene's id
+        this.top_score = 100;
+        this.winner = 'Top Score';
     }
 
     // preload external game assets
     preload() {
-        this.load.path = 'assets/';      // file path
-
-        this.load.image( 'background', 'background.png');
-        this.load.image( 'player', 'player.png');
-        this.load.image( 'enemy', 'enemy.png');
+        this.load.path ='assets/';      // file path
+        this.load.image('background', 'background.png');
+        this.load.image('player', 'player.png');
+        this.load.image('player-0', 'player-0.png');
+        this.load.image('player-1', 'player-1.png');
+        this.load.image('enemy', 'enemy.png');
+        this.load.image('enemy-0', 'enemy-0.png'); 
+        this.load.image('enemy-1', 'enemy-1.png');
+        this.load.image('projectile', 'projectile.png');
+        this.load.image('powerup-projectile', 'powerup-1.png');
+        this.load.image('powerup-slay', 'powerup-2.png');
     }
 
     // create game data
     create() {
+        this.create_hud();
         this.create_map();
+        this.create_projectiles();
+        this.create_animations();
         this.create_player();
         this.create_enemies();
+        this.create_powerups();
         this.create_collisions();
     }
 
     // update game data
-    update() {
-        this.update_player();
+    update(time) {
+        this.update_player(time);
+        this.update_enemies(time);
+        this.update_background();
+        this.update_score();
     }
 
 
@@ -31,16 +46,25 @@ class PlayScene extends Phaser.Scene {
 
 
     // custom helper methods
+
+    create_hud() {
+        this.score = 0;
+        this.score_text = this.add.text(32, 32, "");
+        this.score_text.depth = 3;
+        this.score_text.setColor( 'rgb(255, 255, 255)' ); 
+
+        this.top_score_text = this.add.text( 600, 32, "");
+        this.top_score_text.depth = 3;
+        this.top_score_text.setOrigin(1,0); 
+    }
+
     create_map() {
-        this.add.image(640/2, 480/2, 'background');
+        // this.add.image(640/2, 480/2, 'background');
+        this.background = this.add.tileSprite(640/2, 480/2, 640, 480, 'background');
     }
 
     create_player() {
         this.player = new Player(this);
-    }
-
-    create_collisions() {
-        this.physics.add.overlap(this.player, this.enemies, this.game_over, null, this);
     }
 
     create_enemies() {
@@ -62,13 +86,131 @@ class PlayScene extends Phaser.Scene {
 
         const monster = new Enemy(this, position);  // create new enemy
         this.enemies.push(monster);                 // create instance
+
+        this.score += 1;
     }
 
-    update_player() {
+    slay_enemy(projectile, enemy) {
+        enemy.destroy();
+        projectile.destroy();
+    }
+
+    create_projectiles() {
+        this.player_projectiles = [];
+        this.enemy_projectiles = [];
+    }
+
+    create_powerups() {
+        this.powerups_projectiles = [];
+        this.powerups_slay = [];
+
+        var event = new Object();
+        event.delay = 3000;
+        event.callback = this.spawn_powerup;
+        event.callbackScope = this;
+        event.loop = true;
+        
+        this.time.addEvent(event, this);
+    }
+
+    spawn_powerup() {
+        if ( Phaser.Math.Between(0,4) === 0 ) {
+            const config = new Object();
+            config.y = Phaser.Math.Between(0, 480);
+            config.x = 640+32;
+            config.type = 'powerup-projectile'
+
+            const potion = new PowerUp(this, config);
+            this.powerups_projectiles.push(potion);
+        } else if (Phaser.Math.Between(0,4) === 0 ) {
+            const config = new Object();
+            config.y = Phaser.Math.Between(0, 480);
+            config.x = 640+32;
+            config.type = 'powerup-slay'
+
+            const potion = new PowerUp(this, config);
+            this.powerups_slay.push(potion);
+        }
+    }
+
+    create_animations() {
+        if ( !this.anims.exists('player-move') ){
+            const anim_player_move = new Object();
+            anim_player_move.key = 'player-move';
+            anim_player_move.frameRate = 6;
+            anim_player_move.repeat = -1;
+            anim_player_move.frames = [
+                {key: 'player-0'}, {key: 'player-1'}
+            ];
+            this.anims.create(anim_player_move);    // factory creates anim object
+        }
+        if ( !this.anims.exists('enemy-move') ){
+            const anim_enemy_move = new Object();
+            anim_enemy_move.key = 'enemy-move';
+            anim_enemy_move.frameRate = 6;
+            anim_enemy_move.repeat = -1;
+            anim_enemy_move.frames = [
+                {key: 'enemy-0'}, {key: 'enemy-1'}
+            ];
+            this.anims.create(anim_enemy_move);
+        }
+    }
+    
+    create_collisions() {
+        this.physics.add.overlap(this.player, this.enemies, this.game_over, null, this);
+        this.physics.add.overlap(this.player_projectiles, this.enemies, this.slay_enemy, null, this);
+        this.physics.add.overlap(this.enemy_projectiles, this.player, this.game_over, null, this);
+        this.physics.add.overlap(this.player, this.powerups_slay, this.get_powerup_slay, null, this);
+        this.physics.add.overlap(this.player, this.powerups_projectiles, this.get_powerup_projectile, null, this);
+    }
+
+    get_powerup_slay(player, powerup){
+        this.enemies.forEach( monster => monster.destroy() );
+        this.enemy_projectiles.forEach( bullet => bullet.destroy());
+        powerup.destroy();
+        this.cameras.main.flash();
+    }
+
+    get_powerup_projectile(player, powerup){
+        this.player.bulletScale = Math.min(this.player.bulletScale + 1, 3); 
+        powerup.destroy();
+    }
+
+    // clear_enemies(){
+    //     if (this.enemies.length > 50) {
+
+    //     }
+    // }
+
+
+
+
+
+    update_player(time) {
         this.player.move();
+        this.player.attack(time);
+    }
+
+    update_enemies(time) {
+        this.enemies.forEach(enemy => enemy.attack(time));
+    }
+
+    update_background() {
+        this.background.tilePositionX += 3;
+    }
+
+    update_score() {
+        this.score_text.setText("Score: " + this.score);
+        this.top_score_text.setText(`${this.winner}: ${this.top_score}`);
     }
 
     game_over() {
+        if ( this.score >= this.top_score ) {
+            this.top_score = this.score;
+            this.physics.pause();
+            this.winner = prompt("Winner! Enter your name: ") ?? "Top Score";
+            this.input.keyboard.keys = [];
+        }
         this.cameras.main.flash();
         this.scene.restart();
     }
--- labs/lab-05/src/Player.js
+++ labs/lab-05/src/Player.js
@@ -3,12 +3,18 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         super(scene, 300, 200, 'player');
         this.depth = 2;
         this.speed = 200;
-        scene.add.existing(this);   // add Player object to display
+        this.last_fired = 0;
+        this.projectileScale = 1;
+        this.projectiles = scene.player_projectiles;
 
+        scene.add.existing(this);   // add Player object to display
         scene.physics.add.existing(this);       // add physics body
         this.setCollideWorldBounds(true);
+        this.body.setSize(this.width-16, this.height-16);
+
         this.buttons =                          // scene input manager
-            scene.input.keyboard.addKeys('up,down,left,right');  
+            scene.input.keyboard.addKeys('up,down,left,right,space');
+        this.anims.play('player-move', true);
     }
 
     // player movement
@@ -29,6 +35,27 @@ class Player extends Phaser.Physics.Arcade.Sprite {
         }
         if ( this.buttons.right.isDown ) {
             this.body.velocity.x = this.speed;
+        }
+    }
+
+    attack(time) {
+        if( this.buttons.space.isDown && time - this.last_fired > 400 ) {
+            const position = {x:this.x, y:this.y};
+            const velocity = {x:300, y:0};
+            const projectile = new Projectile(this.scene, position, velocity);
+
+            // POWERUP
+            // scale the sprite you see
+            projectile.setScale(this.projectileScale);
+
+            // POWERUP
+            // make the physics hitbox match what you see (minimal + effective) 
+            projectile.body.setSize(projectile.displayWidth, projectile.displayHeight, true);
+            this.projectiles.push(projectile);
+            this.last_fired = time;
+        }
+        if( this.buttons.space.isUp ) {
+            this.last_fired = 0;
         }
     }
 }
